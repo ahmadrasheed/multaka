b1938c9d8813ce4402ae0552700d6a8ae647f048
Ahmad-Mac <ahmad.rasheed1@yahoo.com>
2019-08-17 14:27:31 +0300

    home page has been coded

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,101 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+<div class="container">                  {{-- Start of Container --}}
+    <div id='section-one'>
+        <div id="main-title" class="row">
+            <div class="col-3">
+                <img src="{{asset('img/multaka-logo.png')}}"  width=180px;>
             </div>
+
+            <div  class="col-9 text-center">
+                <h1 class="title1">أهلا وسهلاً بكم في ملتقى الكتاب</h2>
+                <h2 class="title2">حيث المطالعة والشاي والموسيقى معاً! </h2>
+            </div>
+            
         </div>
     </div>
-</div>
+</div>                                   {{--  End of Container --}}
+
+@endsection
+
+@section('content2')
+<div id="section-two">
+    <h1 class="myfont text-right py-3 bg-purple"> اخر النشاطات والفعاليات في الملتقى</h1>
+    <hr>
+        <div class="row">
+                <div class="card col-md-6 mb-3" > {{-- 1 item --}}
+                        <div class="row no-gutters">
+                          <div class="col-md-6" style="max-width:520px;">
+                            <img src="{{asset('img/multaka-bg.jpg')}}"  height="100%" class="card-img" alt="...">
+                          </div>
+                          <div class="col-md-6">
+                            <div class="card-body text-right">
+                              <h5 class="card-title">الشاعر علي الحلي وقافيته  النونية</h5>
+                              <p class="card-text">سيتم مناقشة الاصدار الاخير من كتاب الشعر العربي للشاعر القدير علي الحلي ونونيته المشهورة وذلك  تمام الساعة السادسة 
+                                  عصرا على قاعة ملتقى الكتاب. الدعوة عامة وللجميع 
+                              </p>
+                              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+
+                      <div class="card col-md-6 mb-3" > {{-- 2item --}}
+                            <div class="row no-gutters">
+                              <div class="col-md-6" style="max-width:520px;">
+                                <img src="{{asset('img/autum.jpg')}}"  height="100%" class="card-img" alt="...">
+                              </div>
+                              <div class="col-md-6">
+                                <div class="card-body text-right">
+                                  <h5 class="card-title">مناظرة ادبية للشاعر الدكتور احمد السالم</h5>
+                                  <p class="card-text">سيتم مناقشة الاصدار الاخير من كتاب الشعر العربي للشاعر القدير علي الحلي ونونيته المشهورة وذلك  تمام الساعة السادسة 
+                                      عصرا على قاعة ملتقى الكتاب. الدعوة عامة وللجميع 
+                                  </p>
+                                  <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
+
+                          <div class="card col-md-6 mb-3" > {{-- 3 item --}}
+                                <div class="row no-gutters">
+                                  <div class="col-md-6" style="max-width:520px;">
+                                    <img src="{{asset('img/bookson.jpg')}}"  height="100%" class="card-img" alt="...">
+                                  </div>
+                                  <div class="col-md-6">
+                                    <div class="card-body text-right">
+                                      <h5 class="card-title">جلسة حوارية بعنوان العراق والمستقبل</h5>
+                                      <p class="card-text">سيتم مناقشة الاصدار الاخير من كتاب الشعر العربي للشاعر القدير علي الحلي ونونيته المشهورة وذلك  تمام الساعة السادسة 
+                                          عصرا على قاعة ملتقى الكتاب. الدعوة عامة وللجميع 
+                                      </p>
+                                      <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    </div>
+                                  </div>
+                                </div>
+                              </div>
+
+                              <div class="card col-md-6 mb-3" > {{-- 4 item --}}
+                                    <div class="row no-gutters">
+                                      <div class="col-md-6" style="max-width:520px;">
+                                        <img src="{{asset('img/arabcoffee.jpg')}}"  height="100%" class="card-img" alt="...">
+                                      </div>
+                                      <div class="col-md-6">
+                                        <div class="card-body text-right">
+                                          <h5 class="card-title">أمسية يقيمها الملتقى احتفاءا بذكر تحرير العراق</h5>
+                                          <p class="card-text">سيتم مناقشة الاصدار الاخير من كتاب الشعر العربي للشاعر القدير علي الحلي ونونيته المشهورة وذلك  تمام الساعة السادسة 
+                                              عصرا على قاعة ملتقى الكتاب. الدعوة عامة وللجميع 
+                                          </p>
+                                          <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                        </div>
+                                      </div>
+                                    </div>
+                                  </div>
+
+
+
+        </div>
+    </div>
+
+
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,63 +18,46 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/main.css') }}" rel="stylesheet">
 </head>
 <body>
-    <div id="app">
-        <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
-            <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
-                    </ul>
-
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Authentication Links -->
-                        @guest
-                            <li class="nav-item">
-                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
-                            </li>
-                            @if (Route::has('register'))
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                                </li>
-                            @endif
-                        @else
-                            <li class="nav-item dropdown">
-                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                    <a class="dropdown-item" href="{{ route('logout') }}"
-                                       onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
-                                    </a>
-
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        @csrf
-                                    </form>
-                                </div>
-                            </li>
-                        @endguest
-                    </ul>
-                </div>
+    <div id="app" class="pic myfont">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <a class="navbar-brand" href="http://ahmadiraq.com"><i class="fas fa-pencil-ruler"></i><img src="{{ asset('img/multaka-logo.png') }}" width="100px"> </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+          
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav mr-auto">
+                <li class="nav-item active">
+                  <a class="nav-link" href="http://ahmadiraq.com">الصفحة الرئيسية<span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#about">عن الملتقى</a>
+                </li>
+                <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    خيارات اخرى
+                  </a>
+                  <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <a class="dropdown-item" href="#">الجلسات</a>
+                    <a class="dropdown-item" href="#">اخر المقالات</a>
+                    <div class="dropdown-divider"></div>
+                    <a class="dropdown-item" href="https://www.facebook.com/ahmad.rasheed.16144">صفحتنا على الفيس بوك</a>
+                  </div>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link " href="#section-2" tabindex="-1" aria-disabled="true"> تحميل تطبيق الموبايل</a>
+                </li>
+              </ul>
+              
             </div>
-        </nav>
+          </nav> 
 
-        <main class="py-4">
-            @yield('content')
-        </main>
+          @yield('content')
     </div>
+
+          @yield('content2')
 </body>
 </html>
